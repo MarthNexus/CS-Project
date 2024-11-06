0463a63daa1eb69e22c0aa9390cefe8c6047e351
MarthNexus <gojesoro223@gmail.com>
2024-11-06 12:05:43 -0600

    Doing scratch

--- Prodject.py
+++ Prodject.py
@@ -1,3 +1,14 @@
 dict = {}
 #we want to use this to be able to store how many categories their weights and what the category is 
+numb_categories = 0
+while numb_categories != "":
+    try:
+        numb_catagories = int(input("enter the number of categories: "))
+    except ValueError:
+        print ("sorry only numbers")
+        continue
+    break
+dict ["categories"] = int(numb_catagories)
+print (dict)
+
 
